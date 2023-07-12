d8b17483561d730e1338fd14c65195d4daab6885
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-07-12 12:10:18 +0530

    bodyparser added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 require('dotenv').config()
 const dbConfig = require("./config/db.config")
-// const bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const userRoute = require("./routes/user.routes")
 const path = require("path");
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 // app.use(express.bodyParser())
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(cookieParser())
 
 
